5921e483a1def1137524dea0b0f5ce6c513a1354
Tejas <tejas75o25@gmail.com>
2022-10-24 13:44:24 +0530

    Added colors and dark mode button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-function App() {
+import React, { useEffect, useState } from "react";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(localStorage.theme);
+
+  const toggleDarkMode = (checked) => {
+    setDarkMode(checked);
+  };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    if (darkMode) {
+      root.classList.add("dark")
+      root.classList.remove("light")
+      localStorage.setItem("theme", "dark")
+    } else {
+      root.classList.add("light")
+      root.classList.remove("dark")
+      localStorage.setItem("theme", "light")
+    }
+  }, [darkMode])
+
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">Hello World!</h1>
-    </div>
+    <div className={`w-full min-h-screen py-14 px-16 dark:bg-my-black bg-my-white ${darkMode && 'dark'}`}>
+      <div div className="w-full h- flex justify-between items-center border-b-2 dark:border-white border-black px-8" >
+        <h1 className="text-9xl font-title dark:text-white">News Feed!</h1>
+        <DarkModeSwitch
+          style={{ marginBottom: '2rem' }}
+          checked={darkMode}
+          onChange={toggleDarkMode}
+          size={80}
+        />
+      </div >
+    </div >
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-white': '#FEFEFE',
+        'my-black': '#262626',
+        'component': {
+          'light': {
+            'badge': '#C4C4C4',
+            'border': '#878787',
+            'bg': '#ECECEC'
+          },
+          'dark': {
+            'badge': '#121212',
+            'border': '#878787',
+            'bg': '#5B5B5B'
+          }
+        }
+      }
+    },
+    fontFamily: {
+      'title': ['Fredericka the Great', 'cursive'],
+      'body': ['Fredoka One', 'cursive'],
+    }
   },
   plugins: [],
 }
