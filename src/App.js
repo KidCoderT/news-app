b2fea42f34e33ccea3a88b42ab2328387cdd2cba
Tejas <62064506+KidCoderT@users.noreply.github.com>
2024-12-11 13:10:56 +0530

    a little brushing up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-import Switch from './components/Switch.js';
-import { Listbox } from '@headlessui/react';
+import Switch from "./components/Switch.js";
+import { Listbox } from "@headlessui/react";
 
 import { AiFillCaretDown as DownIcon } from "react-icons/ai";
 import { GrRefresh as RefreshIcon } from "react-icons/gr";
@@ -13,27 +13,96 @@ import NewsArticle from "./components/NewsArticle.js";
 // todo: make options animate
 
 const countries = [
-  "ae", "ar", "at", "au", "be", "bg",
-  "br", "ca", "ch", "cn", "co", "cu",
-  "cz", "de", "eg", "fr", "gb", "gr",
-  "hk", "hu", "id", "ie", "il", "in",
-  "it", "jp", "kr", "lt", "lv", "ma",
-  "mx", "my", "ng", "nl", "no", "nz",
-  "ph", "pl", "pt", "ro", "rs", "ru",
-  "sa", "se", "sg", "si", "sk", "th",
-  "tr", "tw", "ua", "us", "ve", "za",
-]
-const options = ["general", "entertainment", "business", "health", "science", "sports", "technology"];
+  "ae",
+  "ar",
+  "at",
+  "au",
+  "be",
+  "bg",
+  "br",
+  "ca",
+  "ch",
+  "cn",
+  "co",
+  "cu",
+  "cz",
+  "de",
+  "eg",
+  "fr",
+  "gb",
+  "gr",
+  "hk",
+  "hu",
+  "id",
+  "ie",
+  "il",
+  "in",
+  "it",
+  "jp",
+  "kr",
+  "lt",
+  "lv",
+  "ma",
+  "mx",
+  "my",
+  "ng",
+  "nl",
+  "no",
+  "nz",
+  "ph",
+  "pl",
+  "pt",
+  "ro",
+  "rs",
+  "ru",
+  "sa",
+  "se",
+  "sg",
+  "si",
+  "sk",
+  "th",
+  "tr",
+  "tw",
+  "ua",
+  "us",
+  "ve",
+  "za",
+];
+const options = [
+  "general",
+  "entertainment",
+  "business",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
 
-const getSrc = (country) => `https://raw.githubusercontent.com/madebybowtie/FlagKit/master/Assets/PNG/${country.toUpperCase()}.png`
-const Country = ({ country }) => <><img src={getSrc(country)} alt={country} /> {country.toUpperCase()}</>
+const getSrc = (country) =>
+  `https://raw.githubusercontent.com/madebybowtie/FlagKit/master/Assets/PNG/${country.toUpperCase()}.png`;
+const Country = ({ country }) => (
+  <>
+    <img src={getSrc(country)} alt={country} /> {country.toUpperCase()}
+  </>
+);
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(localStorage.getItem('theme') === "dark");
-  const [selectedCountry, setSelectedCountry] = useState(localStorage.getItem('country') !== null ? localStorage.getItem('country') : countries[0])
-  const [selectedOption, setSelectedOption] = useState(localStorage.getItem('option') !== null ? localStorage.getItem('option') : options[0])
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+  const [selectedCountry, setSelectedCountry] = useState(
+    localStorage.getItem("country") !== null
+      ? localStorage.getItem("country")
+      : "us"
+  );
+  const [selectedOption, setSelectedOption] = useState(
+    localStorage.getItem("option") !== null
+      ? localStorage.getItem("option")
+      : options[0]
+  );
 
-  const [feed, setFeed] = useState([])
+  const [feed, setFeed] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const toggleDarkMode = (checked) => {
     setDarkMode(checked);
@@ -43,57 +112,78 @@ const App = () => {
     const root = window.document.documentElement;
 
     if (darkMode || darkMode === "dark") {
-      root.classList.add("dark")
-      root.classList.remove("light")
-      localStorage.setItem("theme", "dark")
+      root.classList.add("dark");
+      root.classList.remove("light");
+      localStorage.setItem("theme", "dark");
     } else {
-      root.classList.add("light")
-      root.classList.remove("dark")
-      localStorage.setItem("theme", "light")
+      root.classList.add("light");
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-  }, [darkMode])
+  }, [darkMode]);
 
   let refreshFeed = async () => {
-    setFeed([])
+    setFeed([]);
+    setLoading(true);
 
     await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${selectedCountry}&category=${selectedOption}&apiKey=d65aae3dcaa740fd81f972849d16cd04&pageSize=81`,
-      { method: 'GET' }
-    )
-      .then((data) => {
-        data.json().then(result => setFeed(result.articles))
-      })
-    console.log("fetched more data")
-  }
+      `https://newsapi.org/v2/top-headlines?country=${selectedCountry}&apiKey=d65aae3dcaa740fd81f972849d16cd04`,
+      { method: "GET" }
+    ).then((data) => {
+      data.json().then((result) => {
+        console.log(result);
+        setFeed(result.articles);
+        setLoading(false);
+      });
+    });
+    console.log("fetched more data");
+  };
 
   useEffect(() => {
-    localStorage.setItem("country", selectedCountry)
-    localStorage.setItem("option", selectedOption)
-    refreshFeed()
-  }, [selectedCountry, selectedOption])
+    localStorage.setItem("country", selectedCountry);
+    localStorage.setItem("option", selectedOption);
+    refreshFeed();
+  }, [selectedCountry, selectedOption]);
 
   return (
-    <div className={`w-full min-h-screen py-10 px-12 sm:py-14 sm:px-16 dark:bg-my-black bg-my-white`}>
+    <div
+      className={`w-full min-h-screen py-10 px-12 sm:py-14 sm:px-16 dark:bg-my-black bg-my-white`}
+    >
       {/* title */}
-      <div className="w-full flex justify-around sm:justify-between items-center border-b-2 dark:border-white border-black px-8" >
-        <h1 className="text-6xl sm:text-7xl xl:text-8xl font-title dark:text-white">News Feed!</h1>
+      <div className="w-full flex justify-around sm:justify-between items-center border-b-2 dark:border-white border-black px-8">
+        <h1 className="text-6xl sm:text-7xl xl:text-8xl font-title dark:text-white">
+          News Feed!
+        </h1>
         <Switch
           classes="mb-8"
           checked={darkMode}
           onChange={toggleDarkMode}
           size={80}
         />
-      </div >
+      </div>
 
       {/* options */}
-      <div className="w-full flex-wrap flex justify-start items-center px-8 my-3 sm:my-5" >
-        <button onClick={() => { refreshFeed() }} className="mr-3 p-1 rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0  bg-component-light-bg border-2 flex justify-between items-center font-body dark:border-white border-black dark:text-white text-black h-full w-10 flex items-center justify-center">
-          <RefreshIcon size="2em" />
+      <div className="w-full flex-wrap flex justify-start items-center px-8 my-3 sm:my-5">
+        <button
+          onClick={() => {
+            refreshFeed();
+          }}
+          className="mr-3 p-1 rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0  bg-component-light-bg border-2 flex justify-between items-center font-body dark:border-white border-black dark:text-white text-black h-full w-10 flex items-center justify-center"
+        >
+          <RefreshIcon size="2em" color={darkMode ? "white" : "black"} />
         </button>
 
-        <Listbox as={"Div"} value={selectedCountry} onChange={setSelectedCountry} className="w-fit h-full relative mr-3">
-          <Listbox.Button className="p-2 w-24 rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0  bg-component-light-bg border-2 flex justify-between items-center font-body dark:border-white border-black dark:text-white text-black"><DownIcon /><Country country={selectedCountry} /></Listbox.Button>
-          <Listbox.Options className="p-1 z-50 absolute w-[14rem] h-48 scrollbar-thin scrollbar-track-transparent  scrollbar-thumb-gray-700 scrollbar-rounded-md flex flex-wrap border-2 border-black dark:border-white bg-component-light-bg bg-opacity-40 dark:bg-component-dark-bg dark:text-white text-black overflow-y-scroll rounded-md mt-2">
+        <Listbox
+          as={"div"}
+          value={selectedCountry}
+          onChange={setSelectedCountry}
+          className="w-fit h-full relative mr-3"
+        >
+          <Listbox.Button className="p-2 w-24 rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0  bg-component-light-bg border-2 flex justify-between items-center font-body dark:border-white border-black dark:text-white text-black">
+            <DownIcon />
+            <Country country={selectedCountry} />
+          </Listbox.Button>
+          <Listbox.Options className="p-1 z-50 absolute w-[14rem] h-48 scrollbar-thin scrollbar-track-transparent  scrollbar-thumb-gray-700 scrollbar-rounded-md flex flex-wrap border-2 border-black dark:border-white bg-component-light-bg bg-opacity-60 dark:bg-component-dark-bg dark:text-white text-black overflow-y-scroll rounded-md mt-2">
             <div className="absolute w-full h-[210%] top-0 opacity-20 left-0" />
             {countries.map((country) => (
               <Listbox.Option
@@ -102,15 +192,27 @@ const App = () => {
                 disabled={selectedCountry === country}
                 className="cursor-pointer rounded-sm flex p-1 items-center justify-center m-1 font-thin text-sm z-10 "
               >
-                <img src={getSrc(country)} className="w-fit mr-2" alt={country} />
+                <img
+                  src={getSrc(country)}
+                  className="w-fit mr-2"
+                  alt={country}
+                />
                 {country.toUpperCase()}
               </Listbox.Option>
             ))}
           </Listbox.Options>
         </Listbox>
 
-        <Listbox as={"Div"} value={selectedOption} onChange={setSelectedOption} className="w-fit h-full relative">
-          <Listbox.Button className="p-2 w-fit rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0 bg-component-light-bg border-2 flex items-center font-body dark:border-white border-black dark:text-white text-black"><DownIcon /> <span className="ml-2">{selectedOption.toLocaleUpperCase()}</span></Listbox.Button>
+        <Listbox
+          as={"Div"}
+          value={selectedOption}
+          onChange={setSelectedOption}
+          className="w-fit h-full relative"
+        >
+          <Listbox.Button className="p-2 w-fit rounded-md dark:bg-component-dark-bg mt-2 sm:mt-0 bg-component-light-bg border-2 flex items-center font-body dark:border-white border-black dark:text-white text-black">
+            <DownIcon />{" "}
+            <span className="ml-2">{selectedOption.toLocaleUpperCase()}</span>
+          </Listbox.Button>
           <Listbox.Options className="p-1 z-50 w-52 h-fit absolute border-2 border-black dark:border-white bg-component-light-bg bg-opacity-40 dark:bg-component-dark-bg dark:text-white text-black rounded-md mt-1">
             {/* <div className="absolute w-full h-[210%] top-0 opacity-20 left-0" /> */}
             {options.map((option) => (
@@ -125,23 +227,38 @@ const App = () => {
             ))}
           </Listbox.Options>
         </Listbox>
-      </div >
+      </div>
 
       {/* the cards */}
-      <div className="w-full flex-wrap flex justify-center px-1 my-3 sm:my-5 pt-2" >
-        {(feed.length > 0) ? feed.map((article, index) => (
-          <NewsArticle
-            author={article.author}
-            title={article.title}
-            date={article.publishedAt}
-            image={article.urlToImage}
-            url={article.url}
-            key={index}
-          />
-        )) : (<div className="text-3xl font-body text-center text-red-500 mx-6 mt-5">Error!</div>)}
+      <div className="w-full flex-wrap flex justify-center px-1 my-3 sm:my-5 pt-2">
+        {feed.length > 0 ? (
+          feed
+            .filter(
+              (article) =>
+                article.title.length > 0 && article.title !== "[Removed]"
+            )
+            .map((article, index) => (
+              <NewsArticle
+                author={article.author}
+                title={article.title}
+                date={article.publishedAt}
+                image={article.urlToImage}
+                url={article.url}
+                key={index}
+              />
+            ))
+        ) : loading ? (
+          <div className="text-3xl font-body text-center  mx-6 mt-5">
+            Loading
+          </div>
+        ) : (
+          <div className="text-3xl font-body text-center text-red-500 mx-6 mt-5">
+            No News Found
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -1,52 +1,59 @@
-import React from 'react'
+import React from "react";
 import Spinner from "./Spinner.js";
-import placeholder from "./../assets/placeholder.webp"
+import placeholder from "./../assets/placeholder.webp";
 
 const ImageLoader = (props) => {
-    const [loaded, setLoaded] = React.useState(false)
-    const [src, setSrc] = React.useState(props.src)
+  const [loaded, setLoaded] = React.useState(false);
+  const [src, setSrc] = React.useState(props.src);
 
-    return (<div className={`w-full grow relative ${loaded ? 'bg-grey-800' : ''}`}>
-        <img
-            src={src}
-            alt="img"
-            className="absolute top-0 left-0 w-full h-full"
-            onLoad={() => setLoaded(true)}
-            onError={({ currentTarget }) => {
-                // setLoaded(true)
-                currentTarget.onerror = null
-                setSrc(placeholder);
-            }}
-        />
+  return (
+    <div className={`w-full grow relative ${loaded ? "bg-grey-800" : ""}`}>
+      <img
+        src={src ? src : placeholder}
+        alt="img"
+        className="absolute top-0 left-0 w-full h-full"
+        onLoad={() => setLoaded(true)}
+        onError={({ currentTarget }) => {
+          // setLoaded(true)
+          currentTarget.onerror = null;
+          setSrc(placeholder);
+        }}
+      />
 
-        {
-            !loaded && (
-                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-                    <Spinner />
-                </div>
-            )
-        }
-
-    </div>)
-}
+      {!loaded && (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <Spinner />
+        </div>
+      )}
+    </div>
+  );
+};
 
 const NewsArticle = ({ url, title, author, date, image }) => {
-    let publishedAt = new Date(date)
-    let titleSplit = title.split("-");
-    let realTitle = titleSplit[0]
-    let company = titleSplit[titleSplit.length - 1]
+  let publishedAt = new Date(date);
+  let titleSplit = title.split("-");
+  let realTitle = titleSplit[0];
+  let company = titleSplit[titleSplit.length - 1];
 
-    return (
-        <div className="w-[350px] h-[556px] mb-4 mx-4 overflow-hidden border-[5px] bg-component-light-bg dark:bg-component-dark-bg border-component-border rounded-[21px] flex flex-col">
-            <ImageLoader src={image} />
+  return (
+    <div className="w-[350px] h-[556px] mb-4 mx-4 overflow-hidden border-[5px] bg-component-light-bg dark:bg-component-dark-bg border-component-border rounded-[21px] flex flex-col">
+      <ImageLoader src={image} />
 
-            <div className="w-full h-fit px-8 py-7 font-body dark:text-white text-black">
-                <h1 className="text-xl">{realTitle}</h1>
-                <h1 className="font-thin text-base leading-5 ml-3 my-5">Author: {author !== null ? author : "Anonymous"}<br />{publishedAt.toDateString()}<br />{company}</h1>
-                <a className="text-lg my-auto font-title hover:underline" href={url}>Go To Page</a>
-            </div>
-        </div>
-    )
-}
+      <div className="w-full h-fit px-8 py-7 font-body dark:text-white text-black">
+        <h1 className="text-xl">{realTitle}</h1>
+        <h1 className="font-thin text-base leading-5 ml-3 my-5">
+          Author: {author !== null ? author : "Anonymous"}
+          <br />
+          {publishedAt.toDateString()}
+          <br />
+          {company}
+        </h1>
+        <a className="text-lg my-auto font-title hover:underline" href={url}>
+          Go To Page
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default NewsArticle
+export default NewsArticle;
